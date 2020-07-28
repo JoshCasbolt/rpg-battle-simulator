ad55fe33f70a9b210942bdc18dc8d8afb7f11c3f
Josh Casbolt <joshcasbolt@outlook.com>
2020-07-28 19:52:20 +0100

    Basic character image implementation

--- rpg-battle-simulator/src/App.js
+++ rpg-battle-simulator/src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import BattleView from './components/BattleView/BattleView';
-import Villager from './assets/villager.png';
-import Orc from './assets/orc.png';
-
-const gameCharacters = [
-  {
-    type: 'Villager',
-    image: Villager,
-  },
-  {
-    type: 'Orc',
-    image: Orc,
-  },
-];
 
 function App() {
   return (
-    <BattleView characters={gameCharacters} />
+    <BattleView />
   );
 }
 
